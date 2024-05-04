6bfccce6a03478431c475771f69ba4c456c2bfed
EdJ2003 <105022466+EdJ2003@users.noreply.github.com>
2024-05-04 02:50:35 -0500

    Second version of classes inscription

--- src/main/java/com/example/DDDUnisabana/inscripcionClases/application/port/in/IRegistrarClase.java
+++ src/main/java/com/example/DDDUnisabana/inscripcionClases/application/port/in/IRegistrarClase.java
@@ -1,4 +1,8 @@
 package com.example.DDDUnisabana.inscripcionClases.application.port.in;
 
+import com.example.DDDUnisabana.inscripcionClases.application.usecase.ClaseDTO;
+
 public interface IRegistrarClase {
+
+    void registrarClase(ClaseDTO claseDTO);
 }
--- src/main/java/com/example/DDDUnisabana/inscripcionClases/infrastructure/in/controller/ClaseController.java
+++ src/main/java/com/example/DDDUnisabana/inscripcionClases/infrastructure/in/controller/ClaseController.java
@@ -1,19 +1,23 @@
 package com.example.DDDUnisabana.inscripcionClases.infrastructure.in.controller;
 
-import com.example.DDDUnisabana.inscripcionClases.infrastructure.in.dto.ClaseCDTO;
+import com.example.DDDUnisabana.inscripcionClases.application.port.in.IRegistrarClase;
+import com.example.DDDUnisabana.inscripcionClases.application.usecase.ClaseDTO;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/clases")
 public class ClaseController {
 
+   // private final IRegistrarClase registrarClase;
 
-    @PostMapping
-    public void crearClase(@RequestBody ClaseCDTO claseCDTO){
-
+    @PostMapping("/registrar")
+    public void crearClase(@RequestBody ClaseDTO claseDTO){
+        //registrarClase.registrarClase(claseDTO);
     }
 
 }
